b9f7f0e0b78434d6dd23451d2ac38620659f9874
Gary Blackwood <gary@garyblackwood.co.uk>
2021-06-22 00:02:46 +0100

    feat: add dev container

--- next.config.js
+++ next.config.js
@@ -7,6 +7,16 @@ const config = {
   reactStrictMode: true,
   future: undefined,
   experimental: undefined,
+  webpackDevMiddleware: (config) => {
+    if (process.env.DOCKER) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+
+    return config;
+  },
 };
 
 module.exports = config;
