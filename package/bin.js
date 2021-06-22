fc8af24cf93cfdcceb46374ba5bd08eae5293f89
Gary Blackwood <gary@garyblackwood.co.uk>
2021-06-22 22:02:06 +0000

    build: update bin.js

--- package/bin.js
+++ package/bin.js
@@ -4,11 +4,11 @@
 
 const appName = process.argv[2] || "next-starter";
 const { spawnSync } = require("child_process");
+
 const url = "git@github.com:Garee/next-starter";
-
 spawnSync("git", ["clone", url, `${process.cwd()}/${appName}`]);
-spawnSync("npm", ["install", "--prefix", `${process.cwd()}/${appName}`]);
 
-console.log("Ready to go!");
+console.log("Ready, set, go!");
 console.log(`$ cd ${appName}`);
+console.log("$ npm install");
 console.log("$ npm run dev");
